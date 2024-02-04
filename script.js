7dd183a097147c0c152b5ece69e2739b0b55d259
alexander-lipatov <lap1993.12@yandex.ru>
2024-02-04 15:20:03 -0800

    commit

--- script.js
+++ script.js
@@ -1,61 +1,56 @@
-// Задание 3
-// Создать объект, описывающий время (часы, минуты, секун-
-// ды), и следующие функции для работы с этим объектом.
-//      1. Функция вывода времени на экран.
-//      2. Функция изменения времени на переданное количество
-// секунд.
-//      3. Функция изменения времени на переданное количество
-// минут.
-//      4. Функция изменения времени на переданное количество
-// часов.
-// Учтите, что в последних 3-х функциях, при изменении одной
-// части времени, может измениться и другая. Например: если ко
-// времени «20:30:45» добавить 30 секунд, то должно получиться
-// «20:31:15», а не «20:30:75».
+// Задание 1
+// Создать массив «Список покупок». Каждый элемент массива
+// является объектом, который содержит название продукта, необ-
+// ходимое количество и куплен или нет. Написать несколько функ-
+// ций для работы с таким массивом.
+// 1. Вывод всего списка на экран таким образом, чтобы сначала
+// шли некупленные продукты, а потом – купленные.
+// 2. Добавление покупки в список. Учтите, что при добавлении
+// покупки с уже существующим в списке продуктом, необ-
+// ходимо увеличивать количество в существующей покупке,
+// а не добавлять новую.
+// 3. Покупка продукта. Функция принимает название продукта
+// и отмечает его как купленный.
 
-function createNowTime() {
-  const date = new Date();
+function createProductList() {
+  return [];
+}
+
+function productItem(title, qty, isBuy = false) {
   return {
-    hours: date.getHours(),
-    minutes: date.getMinutes(),
-    seconds: date.getSeconds(),
+    title,
+    qty,
+    isBuy,
   };
 }
 
-function printTime(timeObj) {
-  console.log(
-    `Текущее время ${timeObj.hours}:${timeObj.minutes}:${timeObj.seconds}`
-  );
+function printSortList(productList) {
+  productList.sort((a) => (a.isBuy ? 1 : -1));
+  return productList;
 }
 
-function validateTime(timeObj) {
-  if (timeObj.seconds >= 60) {
-    timeObj.minutes += Math.floor(timeObj.seconds / 60);
-    timeObj.seconds = timeObj.seconds % 60;
+function addProduct(list, ...ProductItem) {
+  const isFindProduct = list.find((item) => {
+    if (item.title === ProductItem[0].title) {
+      item.qty += ProductItem[0].qty;
+      return true;
+    }
+  });
+  if (!isFindProduct) {
+    list.push(...ProductItem);
   }
-  if (timeObj.minutes >= 60) {
-    timeObj.hours += Math.floor(timeObj.minutes / 60);
-    timeObj.minutes %= 60;
-  }
-  if (timeObj.hours >= 24) {
-    timeObj.hours %= 24;
-  }
-  return { ...timeObj };
-}
-function addSeconds(timeObj, seconds = 30) {
-  timeObj.seconds += seconds;
-  validateTime(timeObj);
-  return { ...timeObj };
 }
 
-function addMinutes(timeObj, minutes = 30) {
-  timeObj.minutes += minutes;
-  validateTime(timeObj);
-  return { ...timeObj };
+function bayProduct(list, title) {
+  const idxProduct = list.findIndex((product) => product.title == title);
+  list[idxProduct].isBuy = true;
+  return { ...list };
 }
 
-function addHourse(timeObj, hours = 30) {
-  timeObj.hours += hours;
-  validateTime(timeObj);
-  return { ...timeObj };
-}
+const list = createProductList();
+addProduct(list, productItem("apple", 5));
+addProduct(list, productItem("car", 1));
+bayProduct(list, "car");
+bayProduct(list, "apple");
+console.log(printSortList(list));
+console.log(list);
