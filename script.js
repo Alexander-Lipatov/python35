0ba0b7d407ad65353a88bf75bd430e52907d046b
alexander-lipatov <lap1993.12@yandex.ru>
2024-02-04 15:53:19 -0800

    commit

--- script.js
+++ script.js
@@ -1,56 +1,33 @@
-// Задание 1
-// Создать массив «Список покупок». Каждый элемент массива
-// является объектом, который содержит название продукта, необ-
-// ходимое количество и куплен или нет. Написать несколько функ-
-// ций для работы с таким массивом.
-// 1. Вывод всего списка на экран таким образом, чтобы сначала
-// шли некупленные продукты, а потом – купленные.
-// 2. Добавление покупки в список. Учтите, что при добавлении
-// покупки с уже существующим в списке продуктом, необ-
-// ходимо увеличивать количество в существующей покупке,
-// а не добавлять новую.
-// 3. Покупка продукта. Функция принимает название продукта
-// и отмечает его как купленный.
+// Задание 3
+// Создать массив css-стилей (цвет, размер шрифта, выравнива-
+// ние, подчеркивание и т. д.). Каждый элемент массива – это объ-
+// ект, состоящий из двух свойств: название стиля и значение стиля.
+// Написать функцию, которая принимает массив стилей и
+// текст, и выводит этот текст с помощью document.write() в тегах
+// <p></p>, добавив в открывающий тег атрибут style со всеми сти-
+// лями, перечисленными в массиве.
 
-function createProductList() {
-  return [];
+function createStyle() {
+  return [
+    { color: "red" },
+    { "font-size": "50px" },
+    { "text-align": "center" },
+    { "text-decoration": "solid" },
+  ];
 }
 
-function productItem(title, qty, isBuy = false) {
-  return {
-    title,
-    qty,
-    isBuy,
-  };
+function printTextWithStyles(stylesArray, text) {
+    console.log(Object.entries(stylesArray[0]));
+  const styleString = stylesArray
+    .map(
+      (style) =>
+        `${Object.entries(style)
+          .map(([key, value]) => `${key}: ${value};`)
+          .join(" ")}`
+    )
+    .join(" ");
+  document.write(`<p style="${styleString}">${text}</p>`);
 }
 
-function printSortList(productList) {
-  productList.sort((a) => (a.isBuy ? 1 : -1));
-  return productList;
-}
-
-function addProduct(list, ...ProductItem) {
-  const isFindProduct = list.find((item) => {
-    if (item.title === ProductItem[0].title) {
-      item.qty += ProductItem[0].qty;
-      return true;
-    }
-  });
-  if (!isFindProduct) {
-    list.push(...ProductItem);
-  }
-}
-
-function bayProduct(list, title) {
-  const idxProduct = list.findIndex((product) => product.title == title);
-  list[idxProduct].isBuy = true;
-  return { ...list };
-}
-
-const list = createProductList();
-addProduct(list, productItem("apple", 5));
-addProduct(list, productItem("car", 1));
-bayProduct(list, "car");
-bayProduct(list, "apple");
-console.log(printSortList(list));
-console.log(list);
+const stylesArray = createStyle();
+printTextWithStyles(stylesArray, "Hello world!");
