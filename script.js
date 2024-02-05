0980a43d1a22e8ce46c6863f5fe6961c246d4aff
Alexander Lipatov <lap1993.12@yandex.ru>
2024-02-05 21:18:31 +0300

    commit

--- script.js
+++ script.js
@@ -13,13 +13,21 @@ function factorial(n) {
 
 console.log(factorial(5));
 
+const cache = {};
+
 function fibo(n) {
+  console.log(cache);
+  if (cache[n]) return cache[n];
+
   if (n == 1 || n == 2) {
     return 1;
   } else {
-    return fibo(n - 1) + fibo(n - 2);
+    let res = fibo(n - 1) + fibo(n - 2);
+    cache[n] = res;
+    return res;
   }
 }
-console.log(fibo(8));
+
+console.log(fibo(5));
 
 // 1 1 2 3 5 8 13 21
