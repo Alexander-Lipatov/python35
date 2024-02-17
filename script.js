5e8f5aa1efa3a287de6836687bde80b55337dca8
Alexander Lipatov <lap1993.12@yandex.ru>
2024-02-17 22:34:00 +0300

    commit

--- script.js
+++ script.js
@@ -1,63 +1,110 @@
+// Задание 1
+// Реализовать класс, описывающий окружность. В классе должны быть следующие компоненты:
+// ■ поле, хранящее радиус окружности;
+// ■ get-свойство, возвращающее радиус окружности;
+// ■ set-свойство, устанавливающее радиус окружности;
+// ■ get-свойство, возвращающее диаметр окружности;
+// ■ метод, вычисляющий площадь окружности;
+// ■ метод, вычисляющий длину окружности.
+// Продемонстрировать работу свойств и методов.
 
-'use strict'
-function sumTo(n) {
-  return n == 1 ? n : n + sumTo(n - 1);
-}
-console.log(sumTo(100));
+class Circle {
+  canvas = this.createCanvas();
+  constructor(radius) {
+    this._radius = radius;
+  }
+  get radius() {
+    return this._radius;
+  }
+  set radius(newRadius) {
+    this._radius = newRadius;
+    return this._radius;
+  }
+  get diameter() {
+    return 2 * this._radius;
+  }
+  area() {
+    return Math.PI * Math.pow(this._radius, 2);
+  }
+  circumference() {
+    return 2 * Math.PI * this._radius;
+  }
+  createCanvas() {
+    const canvas = document.createElement("canvas");
+    canvas.id = "circle";
+    canvas.width = 300;
+    canvas.height = 300;
+    document.body.insertAdjacentElement("afterbegin", canvas);
+    return canvas;
+  }
+  paintCircle() {
+    let context = this.canvas.getContext("2d");
+    const centerX = this.canvas.width / 2;
+    const centerY = this.canvas.height / 2;
 
-function factorial(n) {
-  if (n == 1) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
+    context.beginPath();
+    context.arc(centerX, centerY, this.radius, 0, this.diameter, false);
+    context.fillStyle = "#ecf110";
+    context.fill();
+    context.lineWidth = 2;
+    context.strokeStyle = "black";
+    context.stroke();
+  }
+  returnInfoCircle() {
+    const ulElement = this.canvas.insertAdjacentElement(
+      "afterend",
+      document.createElement("ul")
+    );
+    const liRadius = document.createElement("li");
+    const liDiameter = document.createElement("li");
+    const liArea = document.createElement("li");
+    const liCircumference = document.createElement("li");
+
+    liRadius.textContent = `Radius: ${this.radius}px`;
+    liDiameter.textContent = `Diameter: ${this.diameter}px`;
+    liArea.textContent = `Area: ${this.area().toFixed(0)}px`;
+    liCircumference.textContent = `Circumference: ${this.circumference().toFixed(
+      0
+    )}px`;
+
+    ulElement.appendChild(liRadius);
+    ulElement.appendChild(liDiameter);
+    ulElement.appendChild(liArea);
+    ulElement.appendChild(liCircumference);
   }
 }
 
-console.log(factorial(5));
+const circle = new Circle(140);
+circle.radius = 100;
+circle.paintCircle();
+circle.returnInfoCircle();
 
-const cache = {};
+// ___________________ЗАДАНИЕ 2_________________________
 
-function fibo(n) {
-  console.log(cache);
-  if (cache[n]) return cache[n];
+// Реализовать класс, описывающий html элемент.
+// Класс HtmlElement должен содержать внутри себя:
+// ■ название тега;
+// ■ самозакрывающийся тег или нет;
+// ■ текстовое содержимое;
+// ■ массив атрибутов;
+// ■ массив стилей;
+// ■ массив вложенных таких же тегов;
+// ■ метод для установки атрибута;
+// ■ метод для установки стиля;
+// ■ метод для добавления вложенного элемента в конец теку- щего элемента;
+// ■ метод для добавления вложенного элемента в начало те-кущего элемента;
+// ■ метод getHtml(), который возвращает html код в виде строки, включая html код вложенных элементов.
 
-  if (n == 1 || n == 2) {
-    return 1;
-  } else {
-    let res = fibo(n - 1) + fibo(n - 2);
-    cache[n] = res;
-    return res;
+class HtmlElement {
+  constructor(tagName, isClosing = false, content = "") {
+    this._tagName = tagName;
+    this._isClosing = isClosing;
+    this._content = content;
+    this._attributes = [];
+    this._styles = [];
+    this._children = []
+  }
+  styleObj(name, value) {
+    return { name, value };
   }
 }
-
-console.log(fibo(5));
-
-// 1 1 2 3 5 8 13 21
-
-let obj = {
-  get propName() {
-    console.log("get");
-    return "Hello World!";
-  },
-  set propName(value) {
-    console.log("set on value", value);
-    return value;
-  },
-};
-
-console.log(obj.propName);
-let o = (obj.propName = 123);
-console.log(o);
-
-const body = document.getElementById("body");
-
-body.addEventListener('click', (e)=>{
-  console.log(this);
-  console.log(e.target);
-},)
-
-
-
-
-// tagName
-
