50f9a65e6e55b96d4c587c9e86c2f79da94de8f0
Alexander Lipatov <lap1993.12@yandex.ru>
2024-03-02 11:43:56 +0300

    123

--- script.js
+++ script.js
@@ -37,17 +37,39 @@ let dragElem;
 // });
 
 manyElem.forEach((elem) => {
-  elem.addEventListener("mousedown", (e) => {
-    console.log(e);
-    let shiftX = e.clientX - ball.getBoundingClientRect().left;
-    let shiftY = e.clientY - ball.getBoundingClientRect().top;
-    ball.style.position = "absolute";
-    ball.style.zIndex = 1000;
-    document.body.append(ball);
-    elem.addEventListener("mausemove", (e) => {
-      console.log(e);
-    });
-  });
+  elem.onmousedown = function(event) { // (1) отследить нажатие
+
+    // (2) подготовить к перемещению:
+    // разместить поверх остального содержимого и в абсолютных координатах
+    elem.style.position = 'absolute';
+    elem.style.zIndex = 1000;
+    // переместим в body, чтобы мяч был точно не внутри position:relative
+    document.body.append(elem);
+    // и установим абсолютно спозиционированный мяч под курсор
+  
+    moveAt(event.pageX, event.pageY);
+  
+    // передвинуть мяч под координаты курсора
+    // и сдвинуть на половину ширины/высоты для центрирования
+    function moveAt(pageX, pageY) {
+      elem.style.left = pageX - elem.offsetWidth / 2 + 'px';
+      elem.style.top = pageY - elem.offsetHeight / 2 + 'px';
+    }
+  
+    function onMouseMove(event) {
+      moveAt(event.pageX, event.pageY);
+    }
+  
+    // (3) перемещать по экрану
+    document.addEventListener('mousemove', onMouseMove);
+  
+    // (4) положить мяч, удалить более ненужные обработчики событий
+    elem.onmouseup = function() {
+      document.removeEventListener('mousemove', onMouseMove);
+      elem.onmouseup = null;
+    };
+  
+  };
 });
 
 ball.onmousedown = function (event) {
