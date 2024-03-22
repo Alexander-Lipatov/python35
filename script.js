2c3f3501938d3c2b952b9ac7c489fc8a3004287f
alexander-lipatov <lap1993.12@yandex.ru>
2024-03-22 18:26:42 +0300

    commit

--- script.js
+++ script.js
@@ -1,43 +1,31 @@
-const searchLoginForm = document.getElementById("search-login");
-const loader = document.querySelector(".loader");
-const description = document.querySelector(".description");
+let position = 1;
+const btnPrev = document.querySelector(".prev");
+const btnNext = document.querySelector(".next");
+const items = document.querySelectorAll(".item");
+const itemsWrap= document.querySelector('.itemsWrap')
 
-searchLoginForm.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const login = searchLoginForm.querySelector("input").value;
-  description.style.display = "none";
-  loader.style.display = "block";
-  const profileData = await getGitProfile(login);
-  searchLoginForm.querySelector("input").value = "";
-  loader.style.display = "none"
-  if (profileData) renderProfileBlock(profileData);
-  if (!profileData) alert("Profile not found!");
+if (position >= items.length-1) btnNext.disabled = true;
+if (position == 0) btnPrev.disabled = true;
+itemsWrap.style.transform = `translateX(-${position * 100}%)`;
+
+btnPrev.addEventListener("click", (e) => {
+  position--;
+  itemsWrap.style.transform = `translateX(-${position * 100}%)`;
+  console.log(position);
+  if (position == 0) {
+    btnPrev.disabled = true;
+  } else {
+    btnNext.disabled = false;
+  }
 });
 
-async function getGitProfile(login) {
-  const data = await fetch(`https://api.github.com/users/${login}`);
-  if (!data.ok) return null;
-  return await data.json();
-}
-
-function renderProfileBlock(data) {
-  const profileBlockHtml = document.querySelector("main");
-  const html = `
-    <div class="leftBlock">
-      <img width="25%" src="${data.avatar_url}" />
-      <div><span>Name:</span><span>${data.name}</span></div>
-      <div><span>Login:</span><span>${data.login}</span></div>
-    </div>
-    <div class="rightBlock">
-      <div><span>Url to git:</span> <a href="${data.html_url}">${data.html_url}</a></div>
-      <div><span>Blog:</span><span>${data.blog}</span></div>
-      <div><span>City:</span><span>${data.location}</span></div>
-      <div><span>Email:</span><span>${data.email}</span></div>
-      <div>
-        <div><span>Followers:</span><span>${data.followers}</span></div>
-        <div><span>Following:</span><span>${data.following}</span></div>
-      </div>
-    </div>
-  `;
-  profileBlockHtml.innerHTML = html;
-}
+btnNext.addEventListener("click", (e) => {
+  position++;
+  itemsWrap.style.transform = `translateX(-${position * 100}%)`;
+  console.log(position);
+  if (position >= items.length-1) {
+    btnNext.disabled = true;
+  } else {
+    btnPrev.disabled = false;
+  }
+});
